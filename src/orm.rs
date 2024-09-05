89f82155c4dd4cd81d831a235ea29a0e269eea9d
miky-rola <mikyrola8@gmail.com>
2024-09-05 06:44:31 +0000

    refactor: orm, connection to db

--- src/orm.rs
+++ src/orm.rs
@@ -3,8 +3,9 @@ use diesel::pg::PgConnection;
 use uuid::Uuid;
 use crate::models::{User, Blog, Comment, Like};
 use crate::schema::{users, blogs, comments, likes};
+use crate::orm::{get_comment, update_comment, delete_comment, get_like};
 
-pub fn create_user(conn: &PgConnection, username: &str, email: &str, password_hash: &str) -> Result<User, diesel::result::Error> {
+pub fn create_user(conn: &mut PgConnection, username: &str, email: &str, password_hash: &str) -> Result<User, diesel::result::Error> {
     let new_user = NewUser {
         username,
         email,
@@ -16,11 +17,11 @@ pub fn create_user(conn: &PgConnection, username: &str, email: &str, password_ha
         .get_result(conn)
 }
 
-pub fn get_user(conn: &PgConnection, user_id: Uuid) -> Result<User, diesel::result::Error> {
+pub fn get_user(conn: &mut PgConnection, user_id: Uuid) -> Result<User, diesel::result::Error> {
     users::table.find(user_id).get_result::<User>(conn)
 }
 
-pub fn update_user(conn: &PgConnection, user_id: Uuid, username: &str, email: &str) -> Result<User, diesel::result::Error> {
+pub fn update_user(conn: &mut PgConnection, user_id: Uuid, username: &str, email: &str) -> Result<User, diesel::result::Error> {
     diesel::update(users::table.find(user_id))
         .set((
             users::username.eq(username),
@@ -29,12 +30,12 @@ pub fn update_user(conn: &PgConnection, user_id: Uuid, username: &str, email: &s
         .get_result::<User>(conn)
 }
 
-pub fn delete_user(conn: &PgConnection, user_id: Uuid) -> Result<usize, diesel::result::Error> {
+pub fn delete_user(conn: &mut PgConnection, user_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(users::table.find(user_id))
         .execute(conn)
 }
 
-pub fn create_blog(conn: &PgConnection, title: &str, content: &str, author_id: Uuid) -> Result<Blog, diesel::result::Error> {
+pub fn create_blog(conn: &mut PgConnection, title: &str, content: &str, author_id: Uuid) -> Result<Blog, diesel::result::Error> {
     let new_blog = NewBlog {
         title,
         content,
@@ -46,11 +47,11 @@ pub fn create_blog(conn: &PgConnection, title: &str, content: &str, author_id: U
         .get_result(conn)
 }
 
-pub fn get_blog(conn: &PgConnection, blog_id: Uuid) -> Result<Blog, diesel::result::Error> {
+pub fn get_blog(conn: &mut PgConnection, blog_id: Uuid) -> Result<Blog, diesel::result::Error> {
     blogs::table.find(blog_id).get_result::<Blog>(conn)
 }
 
-pub fn update_blog(conn: &PgConnection, blog_id: Uuid, title: &str, content: &str) -> Result<Blog, diesel::result::Error> {
+pub fn update_blog(conn: &mut PgConnection, blog_id: Uuid, title: &str, content: &str) -> Result<Blog, diesel::result::Error> {
     diesel::update(blogs::table.find(blog_id))
         .set((
             blogs::title.eq(title),
@@ -60,12 +61,12 @@ pub fn update_blog(conn: &PgConnection, blog_id: Uuid, title: &str, content: &st
         .get_result::<Blog>(conn)
 }
 
-pub fn delete_blog(conn: &PgConnection, blog_id: Uuid) -> Result<usize, diesel::result::Error> {
+pub fn delete_blog(conn: &mut PgConnection, blog_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(blogs::table.find(blog_id))
         .execute(conn)
 }
 
-pub fn create_comment(conn: &PgConnection, blog_id: Uuid, user_id: Uuid, content: &str, parent_comment_id: Option<Uuid>) -> Result<Comment, diesel::result::Error> {
+pub fn create_comment(conn: &mut PgConnection, blog_id: Uuid, user_id: Uuid, content: &str, parent_comment_id: Option<Uuid>) -> Result<Comment, diesel::result::Error> {
     let new_comment = NewComment {
         blog_id,
         user_id,
@@ -78,22 +79,22 @@ pub fn create_comment(conn: &PgConnection, blog_id: Uuid, user_id: Uuid, content
         .get_result(conn)
 }
 
-pub fn get_comment(conn: &PgConnection, comment_id: Uuid) -> Result<Comment, diesel::result::Error> {
+pub fn get_comment(conn: &mut PgConnection, comment_id: Uuid) -> Result<Comment, diesel::result::Error> {
     comments::table.find(comment_id).get_result::<Comment>(conn)
 }
 
-pub fn update_comment(conn: &PgConnection, comment_id: Uuid, content: &str) -> Result<Comment, diesel::result::Error> {
+pub fn update_comment(conn: &mut PgConnection, comment_id: Uuid, content: &str) -> Result<Comment, diesel::result::Error> {
     diesel::update(comments::table.find(comment_id))
         .set(comments::content.eq(content))
         .get_result::<Comment>(conn)
 }
 
-pub fn delete_comment(conn: &PgConnection, comment_id: Uuid) -> Result<usize, diesel::result::Error> {
+pub fn delete_comment(conn: &mut PgConnection, comment_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(comments::table.find(comment_id))
         .execute(conn)
 }
 
-pub fn create_like(conn: &PgConnection, blog_id: Uuid, user_id: Uuid) -> Result<Like, diesel::result::Error> {
+pub fn create_like(conn: &mut PgConnection, blog_id: Uuid, user_id: Uuid) -> Result<Like, diesel::result::Error> {
     let new_like = NewLike {
         blog_id,
         user_id,
@@ -104,11 +105,11 @@ pub fn create_like(conn: &PgConnection, blog_id: Uuid, user_id: Uuid) -> Result<
         .get_result(conn)
 }
 
-pub fn get_like(conn: &PgConnection, like_id: Uuid) -> Result<Like, diesel::result::Error> {
+pub fn get_like(conn: &mut PgConnection, like_id: Uuid) -> Result<Like, diesel::result::Error> {
     likes::table.find(like_id).get_result::<Like>(conn)
 }
 
-pub fn delete_like(conn: &PgConnection, like_id: Uuid) -> Result<usize, diesel::result::Error> {
+pub fn delete_like(conn: &mut PgConnection, like_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(likes::table.find(like_id))
         .execute(conn)
 }
