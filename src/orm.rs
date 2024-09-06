bee950a1a66de2b5e671ed15f07ef331afe21f98
miky-rola <mikyrola8@gmail.com>
2024-09-06 16:29:40 +0000

    fixed orm unused functions

--- src/orm.rs
+++ src/orm.rs
@@ -5,6 +5,7 @@ use crate::models::{User, Blog, Comment, Like};
 use crate::schema::{users, blogs, comments, likes};
 // use crate::orm::{ update_comment, delete_comment, get_like};
 
+#[allow(dead_code)]
 pub fn create_user(conn: &mut PgConnection, username: &str, email: &str, password_hash: &str) -> Result<User, diesel::result::Error> {
     let new_user = NewUser {
         username,
@@ -17,10 +18,12 @@ pub fn create_user(conn: &mut PgConnection, username: &str, email: &str, passwor
         .get_result(conn)
 }
 
+#[allow(dead_code)]
 pub fn get_user(conn: &mut PgConnection, user_id: Uuid) -> Result<User, diesel::result::Error> {
     users::table.find(user_id).get_result::<User>(conn)
 }
 
+#[allow(dead_code)]
 pub fn update_user(conn: &mut PgConnection, user_id: Uuid, username: &str, email: &str) -> Result<User, diesel::result::Error> {
     diesel::update(users::table.find(user_id))
         .set((
@@ -30,11 +33,15 @@ pub fn update_user(conn: &mut PgConnection, user_id: Uuid, username: &str, email
         .get_result::<User>(conn)
 }
 
+
+#[allow(dead_code)]
 pub fn delete_user(conn: &mut PgConnection, user_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(users::table.find(user_id))
         .execute(conn)
 }
 
+
+#[allow(dead_code)]
 pub fn create_blog(conn: &mut PgConnection, title: &str, content: &str, author_id: Uuid) -> Result<Blog, diesel::result::Error> {
     let new_blog = NewBlog {
         title,
@@ -47,10 +54,12 @@ pub fn create_blog(conn: &mut PgConnection, title: &str, content: &str, author_i
         .get_result(conn)
 }
 
+#[allow(dead_code)]
 pub fn get_blog(conn: &mut PgConnection, blog_id: Uuid) -> Result<Blog, diesel::result::Error> {
     blogs::table.find(blog_id).get_result::<Blog>(conn)
 }
 
+#[allow(dead_code)]
 pub fn update_blog(conn: &mut PgConnection, blog_id: Uuid, title: &str, content: &str) -> Result<Blog, diesel::result::Error> {
     diesel::update(blogs::table.find(blog_id))
         .set((
@@ -61,11 +70,15 @@ pub fn update_blog(conn: &mut PgConnection, blog_id: Uuid, title: &str, content:
         .get_result::<Blog>(conn)
 }
 
+
+#[allow(dead_code)]
 pub fn delete_blog(conn: &mut PgConnection, blog_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(blogs::table.find(blog_id))
         .execute(conn)
 }
 
+
+#[allow(dead_code)]
 pub fn create_comment(conn: &mut PgConnection, blog_id: Uuid, user_id: Uuid, content: &str, parent_comment_id: Option<Uuid>) -> Result<Comment, diesel::result::Error> {
     let new_comment = NewComment {
         blog_id,
@@ -79,21 +92,27 @@ pub fn create_comment(conn: &mut PgConnection, blog_id: Uuid, user_id: Uuid, con
         .get_result(conn)
 }
 
+
+#[allow(dead_code)]
 pub fn get_comment(conn: &mut PgConnection, comment_id: Uuid) -> Result<Comment, diesel::result::Error> {
     comments::table.find(comment_id).get_result::<Comment>(conn)
 }
 
+
+#[allow(dead_code)]
 pub fn update_comment(conn: &mut PgConnection, comment_id: Uuid, content: &str) -> Result<Comment, diesel::result::Error> {
     diesel::update(comments::table.find(comment_id))
         .set(comments::content.eq(content))
         .get_result::<Comment>(conn)
 }
 
+#[allow(dead_code)]
 pub fn delete_comment(conn: &mut PgConnection, comment_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(comments::table.find(comment_id))
         .execute(conn)
 }
 
+#[allow(dead_code)]
 pub fn create_like(conn: &mut PgConnection, blog_id: Uuid, user_id: Uuid) -> Result<Like, diesel::result::Error> {
     let new_like = NewLike {
         blog_id,
@@ -105,10 +124,13 @@ pub fn create_like(conn: &mut PgConnection, blog_id: Uuid, user_id: Uuid) -> Res
         .get_result(conn)
 }
 
+#[allow(dead_code)]
 pub fn get_like(conn: &mut PgConnection, like_id: Uuid) -> Result<Like, diesel::result::Error> {
     likes::table.find(like_id).get_result::<Like>(conn)
 }
 
+
+#[allow(dead_code)]
 pub fn delete_like(conn: &mut PgConnection, like_id: Uuid) -> Result<usize, diesel::result::Error> {
     diesel::delete(likes::table.find(like_id))
         .execute(conn)
